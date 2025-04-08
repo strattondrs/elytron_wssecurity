c4dc9412107a865bc547e1fa27624158eaab3fbb
Daniel Stratton <Daniel.Stratton@response-systems.com>
2025-04-08 17:17:20 +1000

    It's ... working?  Time to figure out the minimum required

--- ejb/src/main/java/com/example/soap/MySoapService.java
+++ ejb/src/main/java/com/example/soap/MySoapService.java
@@ -3,23 +3,18 @@ package com.example.soap;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.ejb.Stateless;
+import javax.annotation.security.PermitAll;
 
 import org.apache.cxf.interceptor.InInterceptors;
 import org.apache.cxf.annotations.Policy;
 import org.jboss.ws.api.annotation.EndpointConfig;
 
 @WebService
-// So this changes the namespace of the method from the reverse of our java package (e.g. soap.example.com)
-// to whatever is nominated here
-//( targetNamespace = "http://www.jboss.org/jbossws/ws-extensions/wssecuritypolicy" )
 @Stateless
 @EndpointConfig(configFile = "META-INF/jaxws-endpoint-config.xml", configName = "Custom WS-Security Endpoint")
 @Policy(placement = Policy.Placement.BINDING, uri = "META-INF/JavaFirstPolicy.xml")
-@InInterceptors(interceptors = {
-      "org.jboss.wsf.stack.cxf.security.authentication.SubjectCreatingPolicyInterceptor"
-//      "org.jboss.wsf.stack.cxf.security.authentication.SubjectCreatingInterceptor"
-}
-)
+@InInterceptors(interceptors = { "org.jboss.wsf.stack.cxf.security.authentication.SubjectCreatingPolicyInterceptor" })
+@PermitAll
 public class MySoapService {
 
     @WebMethod

689d46a96baa9dd6166ae8771da780c0ed75df2b
Daniel Stratton <Daniel.Stratton@response-systems.com>
2025-04-08 11:28:31 +1000

    Annotate the class with an interceptor.  Set a security domain on the EAR

--- ejb/src/main/java/com/example/soap/MySoapService.java
+++ ejb/src/main/java/com/example/soap/MySoapService.java
@@ -4,8 +4,15 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.ejb.Stateless;
 
+import org.apache.cxf.interceptor.InInterceptors;
+import org.jboss.ws.api.annotation.EndpointConfig;
+
 @WebService
 @Stateless
+//@EndpointConfig(configFile = "META-INF/jaxws-endpoint-config.xml", configName = "Custom WS-Security Endpoint")
+@InInterceptors(interceptors = {
+      "org.jboss.wsf.stack.cxf.security.authentication.SubjectCreatingPolicyInterceptor"}
+)
 public class MySoapService {
 
     @WebMethod
